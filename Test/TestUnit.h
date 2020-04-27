153d5baca4c06fb076cdedb26ef5284b46a232a3
guenoleF <guenole.fiche@gmail.com>
2020-04-27 17:16:28 +0200

    Guenole 27/04 Test Unit sans les trucs bizarres

--- Test/TestUnit.h
+++ Test/TestUnit.h
@@ -18,19 +18,6 @@ using namespace std;
 #define TEST_DECL(x) void test_##x()
 
 
-#define TEST_ADD(name) \
-	suite->addTest(new CppUnit::TestCaller<TestAssociation>("test_"#name, \
-		&TestAssociation::test_##name));
-    suite->addTest(new CppUnit::TestCaller<TestSession>("test_"#name, \
-		&TestSession::test_##name));
-    suite->addTest(new CppUnit::TestCaller<TestCollecteurCSV>("test_"#name, \
-		&TestCollecteurCSV::test_##name));
-    suite->addTest(new CppUnit::TestCaller<TestSessionAdmin>("test_"#name, \
-		&TestSessionAdmin::test_##name));
-    suite->addTest(new CppUnit::TestCaller<TestSessionMembre>("test_"#name, \
-		&TestSessionMembre::test_##name));
-    //suite->addTest(new CppUnit::TestCaller<TestSessionConsultant>("test_"#name, \
-		&TestConsultantSession::test_##name));
 
 
 #endif /* TEST_UNIT_H */
