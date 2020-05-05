8564b21bf437f441f5895af7d72b49b0735fb2b7
guenoleF <guenole.fiche@gmail.com>
2020-05-05 09:12:39 +0200

    Guenole 05/05 PDF de tout le monde + CMakeLists

--- ProjetMain/Projet.cpp
+++ ProjetMain/Projet.cpp
@@ -6,9 +6,13 @@
 
 
 int main(int argc, char *argv[]) {
-    Consultant * pConsultant = new Consultant( "Renault", "Fiche", "Guénolé");
+    Consultant * pConsultant = new Consultant( "Renault", "Fiche", "Guenole");
+    Membre * pMembre = new Membre("Devco" , "Marsot", "Corentin");
 
-    RenduPDF rendu = RenduPDF( "/tmp/test.pdf");
-    rendu.Fiche(pConsultant);
+    RenduPDF rendu1 = RenduPDF( "/tmp/testConsultant.pdf");
+    rendu1.Generer_membre(pConsultant);
+    RenduPDF rendu2 = RenduPDF("/tmp/testMembre.pdf");
+    rendu2.Generer_membre(pMembre);
+
     return 0;
 }
