6eb5344633de32cdc1105b32ba01d813c6048e99
guenoleF <guenole.fiche@gmail.com>
2020-05-05 12:49:58 +0200

    Guenole 05/05 Ya tout qui marche

--- ProjetMain/Projet.cpp
+++ ProjetMain/Projet.cpp
@@ -6,13 +6,28 @@
 
 
 int main(int argc, char *argv[]) {
-    Consultant * pConsultant = new Consultant( "Renault", "Fiche", "Guenole");
-    Membre * pMembre = new Membre("Devco" , "Marsot", "Corentin");
 
-    RenduPDF rendu1 = RenduPDF( "/tmp/testConsultant.pdf");
-    rendu1.Generer_membre(pConsultant);
-    RenduPDF rendu2 = RenduPDF("/tmp/testMembre.pdf");
-    rendu2.Generer_membre(pMembre);
+
+
+    Association test = Gestion_membre::Load("AJIR");
+    RenduPDF rendu = RenduPDF("/tmp/Fiche" + test.getNom() + ".pdf");
+    rendu.Generer_Association(test);
+
+
+    for (int i =0 ; i<test.getNombre_Membres() ; i++ ){
+        Membre *p_test = new Membre(test.getMembres()[i].getpole());
+        p_test->setNotes(test.getMembres()[i].getNotes());
+        rendu = RenduPDF("/tmp/Fiche " + test.getMembres()[i].getPrenom() + " " + test.getMembres()[i].getNom() + ".pdf");
+        rendu.Generer_membre(p_test);
+    }
+
+
+    for (int i =0 ; i<=test.getNombre_Consultants();  i++ ){
+        Consultant *p_test = new Consultant(test.getConsultant()[i].getMission());
+        p_test->setNotes(test.getConsultant()[i].getNotes());
+        rendu = RenduPDF("/tmp/Fiche " + test.getConsultant()[i].getPrenom() + " " + test.getConsultant()[i].getNom() + ".pdf");
+        rendu.Generer_membre(p_test);
+    }
 
 
     return 0;
